6b0c972ab166bb5ebe4ca78fa578b2493c03f293
Jiakun Xu <jiakunxu@gmail.com>
2020-04-20 15:00:06 +0800

    app

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+const sdk = require('/vendor/wafer2-client-sdk/index')
+const config = require('./config')
+
 App({
   onLaunch: function () {
+    sdk.setLoginUrl(config.service.loginUrl);
 
+    sdk.loginWithCode({
+      method: "POST",
+      success: res => {
+        this.globalData.userInfo = res;
+
+        if (this.userInfoReadyCallback) {
+          this.userInfoReadyCallback(this.globalData)
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
   }
 })
