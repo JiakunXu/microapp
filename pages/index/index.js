6e4c6469fbbd80d31b42f2b484488dbc8e692db3
Jiakun Xu <jiakunxu@gmail.com>
2020-04-20 15:00:21 +0800

    Update index.js

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,31 @@
+const sdk = require('../../vendor/wafer2-client-sdk/index')
+const config = require('../../config')
+const util = require('../../utils/util.js')
+
 const app = getApp()
 
 Page({
   data: {
-
+    userInfo: {}
   },
   onLoad: function () {
-    console.log('Welcome to Mini Code')
+    this.getUserInfo();
   },
+
+  getUserInfo: function (e) {
+    const that = this;
+    sdk.login({
+      method: "POST",
+      success: res => {
+        app.globalData.userInfo = res;
+
+        that.setData({
+          userInfo: res
+        })
+      },
+      fail: err => {
+        util.showModel('温馨提示', err.message)
+      }
+    })
+  }
 })
