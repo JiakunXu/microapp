d70d9cf5d0cb76520bfec6706a133013dcc16116
Jiakun Xu <jiakunxu@gmail.com>
2020-04-20 20:36:24 +0800

    demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,13 @@ const app = getApp()
 
 Page({
   data: {
-    userInfo: {}
+    userInfo: {},
+
+    loading: false,
+    finished: false,
+    pageNo: 1,
+    pageSize: 10,
+    list: []
   },
   onLoad: function () {
     if (app.globalData.userInfo) {
@@ -20,5 +26,66 @@ Page({
         })
       }
     }
+
+    this.load();
+  },
+  onPullDownRefresh: function () {
+    this.setData({
+      finished: false,
+      pageNo: 1
+    });
+
+    this.load();
+  },
+  onReachBottom: function () {
+    this.load();
+  },
+
+  load: function () {
+    this._list({});
+  },
+  _list: function (data) {
+    if (this.data.finished) {
+      return;
+    }
+
+    this.setData({
+      loading: true
+    });
+
+    const that = this;
+    sdk.request({
+      url: config.service.baseUrl + '/api/deliveryOrder/list',
+      data: {
+        ...data,
+        pageNo: this.data.pageNo,
+        pageSize: this.data.pageSize
+      },
+      success(result) {
+        wx.stopPullDownRefresh();
+
+        if (result.data.code == "10000") {
+          that.setData({
+            list: that.data.pageNo === 1 ? result.data.list : that.data.list.concat(result.data.list),
+            pageNo: that.data.pageNo + 1,
+          });
+        }
+
+        // 加载状态结束
+        that.setData({
+          loading: false
+        });
+
+        // 数据全部加载完成
+        if (!result.data.list || result.data.list.length < that.data.pageSize) {
+          that.setData({
+            finished: true
+          });
+        }
+      },
+      fail(error) {
+        util.showModel('请求失败', error)
+      }
+    });
   }
 })
